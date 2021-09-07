ef244e10fe7caa03ac0c2dbc369123959bfc4442
AMFahim <amfahim2019@gmail.com>
2021-09-07 10:05:51 +0600

    footer section done

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import './Footer.css';
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { Link } from "@material-ui/core";
 
 const Footer = () => {
   return (
-    <div className="container">
-      <h1>This is footer section.</h1>
-    </div>
+    <MDBFooter color="blue" className="font-small pt-4 mt-4 bg-dark text-white">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="4">
+            <h1>NEWS PIT</h1>
+          </MDBCol>
+          <MDBCol md="4">
+          <a className="footerIcon" target="_blank" rel="noreferrer" href="https://www.facebook.com/" ><FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon></a>
+              <a className="footerIcon" target="_blank" rel="noreferrer" href="https://www.twitter.com/" ><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a>
+                <a className="footerIcon" target="_blank" rel="noreferrer" href="https://www.linkedin.com/" ><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a>
+                <a className="footerIcon" target="_blank" rel="noreferrer" href="https://www.instagram.com/" ><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></a>
+          </MDBCol>
+          <MDBCol md="4">
+            <h5 className="title for-US">News Pit</h5>
+            <ul>
+              <li className="list-unstyled">
+                <Link className="text-white" href="#!">About</Link>
+              </li>
+              <li className="list-unstyled">
+                <Link className="text-white" href="#!">News</Link>
+              </li>
+              <li className="list-unstyled">
+                <Link className="text-white" href="#!">Hot Topics</Link>
+              </li>
+              <li className="list-unstyled">
+                <Link className="text-white" href="#!">Contact Us</Link>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright: <a className="text-white" style={{textDecoration:"none"}} href="https://abdul-mozid-fahim.netlify.app/"> Abdul Mozid Fahim </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
   );
-};
+}
 
 export default Footer;
